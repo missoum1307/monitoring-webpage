2439f2f161b25cf0f9a5553097547e4438dca4a7
missoum1307 <missoum1307@users.noreply.github.com>
2020-05-17 03:20:29 +0200

    fix process.env

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const puppeteer = require('puppeteer')
 const CronJob = require('cron').CronJob
 const sgMail = require('@sendgrid/mail')
 
-const cookie = process.env.COOKIE
+const googleSweet = JSON.parse(process.env.googleCookie)
 const port = process.env.PORT || 3000 
 const url = process.env.urlDbProd
 const sendGridApiKey = process.env.sendGrid_api_key
 sgMail.setApiKey(sendGridApiKey)
+
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 const db = mongoose.connection;
 db.on('error', (err)=> {
@@ -21,25 +22,29 @@ db.once('open', () => {
 })
 
 const Schema = mongoose.Schema
-const messageSchema = new Schema({ url: String, pageLength: Number})
+const messageSchema = new Schema({ url: String, pageLength: Number, cookie: String})
 const urlDb = mongoose.model('Url', messageSchema)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
+
 })
+
 app.post('/url', (req, res) => {
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
-    await page.setCookie(cookie);
-    // const cookie = await page.cookies('http://google.com');
+    const cookieShow = await page.cookies('http://google.com');
+    await page.setCookie(googleSweet[0])    
     const request = await page.goto(req.body.url)
-    //console.log(cookie)
-    const url = new urlDb({ url: req.body.url, pageLength: request.headers()['content-length']})
+    const bufferContent =  await request.buffer()
+    const contentLength =  request.headers()['content-length']
+    console.log(contentLength, bufferContent.length)
+    const url = new urlDb({ url: req.body.url, pageLength: contentLength ? contentLength : bufferContent.length, cookie: req.body.cookie})
     try {
         await url.save()
         console.log('url has been added to Database')
@@ -54,7 +59,7 @@ app.post('/url', (req, res) => {
 
 })
 
-const job = new CronJob('1 * * * * *', () => {
+const job = new CronJob('* * */6 * * *', () => {
 
     urlDb.find({}, (err, data) => {
         
@@ -62,10 +67,13 @@ const job = new CronJob('1 * * * * *', () => {
             let docsCount = await urlDb.countDocuments()
             for (i = 0; i < docsCount; i++) {
 
-                const browser = await puppeteer.launch();
+                const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
                 const page = await browser.newPage();
                 const request = await page.goto(data[i].url);
-                if (request.headers()['content-length'] != data[i].pageLength ) {
+                const bufferContent =  await request.buffer()
+                const contentLength =  request.headers()['content-length']
+                //console.log(contentLength, bufferContent.length)
+                if ((contentLength ? contentLength : bufferContent.length) != data[i].pageLength ) {
                   sgMail.send({
                     to: 'missoumozil@gmail.com',
                     from: 'missoumxss@gmail.com',
@@ -73,7 +81,7 @@ const job = new CronJob('1 * * * * *', () => {
                     text: `A change has been made on ${data[i].url} from ${data[i].pageLength} to ${request.headers()['content-length']}`
                   })
                   console.log(`A change has been made on ${data[i].url} from ${data[i].pageLength} to ${request.headers()['content-length']}`)
-                  await urlDb.updateOne({url: data[i].url}, {pageLength: request.headers()['content-length']});
+                  await urlDb.updateOne({url: data[i].url}, {pageLength: contentLength ? contentLength : bufferContent.length});
                 }
                 await browser.close();
                 
